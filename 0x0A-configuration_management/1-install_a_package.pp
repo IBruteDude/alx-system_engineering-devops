48b0bb7aab03a41cc957e91b9e1ae509fb54c5fa
IBruteDude <kamarf505@gmail.com>
2023-09-23 02:55:12 +0000

    executing a command, AlHamdulillah

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # installs flask on the server
 package { 'pipflask':
-  name        => 'flask',
-  provider    => 'pip3',
-  ensure      => '2.1.0',
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3',
 }
