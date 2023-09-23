c5e6b39562306ce53fb422620a48c2dc4a430e80
IBruteDude <kamarf505@gmail.com>
2023-09-22 19:21:16 -0700

    Bismillah

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# create a temporary 'school' file with attributes
+# Creates a file with content
 file { '/tmp/school':
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
