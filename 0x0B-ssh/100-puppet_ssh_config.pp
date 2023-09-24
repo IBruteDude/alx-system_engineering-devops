2aa3e218b07f73fa9512a0e4665c9c8e02e227c8
IBruteDude <kamarf505@gmail.com>
2023-09-24 21:15:01 +0000

    trying to configure it right, Bismillah

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,13 @@
 # install puppet module
-
-exec { 'install module':
-	command => 'puppet module install puppetlabs-stdlib',
-}
-
 include stdlib
 
 file_line {
-  'ensure pwd off':
+  'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '   PasswordAuthentication no',
   ;
- 'ensure private key path correct':
+ 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '   IdentityFile ~/.ssh/school',
