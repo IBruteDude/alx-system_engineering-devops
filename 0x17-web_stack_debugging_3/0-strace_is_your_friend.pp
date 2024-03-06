824b694c421c8a77b57fac5d8b77b7f6bc04f0c5
IBruteDude <kamarf505@gmail.com>
2024-03-06 17:53:07 +0200

    Added an explanatory comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
-exec { 'refresh_cache':
+# Changes the false file name causing server error
+exec { 'edit_false_file_name':
     command => '/usr/bin/env sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php',
 }
